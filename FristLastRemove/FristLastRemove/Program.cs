bbd9298cebd734ed93787606588707b69019a5ea
Kovacs.Alexandra.E <Kovacs.Alexandra.E@O16G07>
2020-01-27 08:59:24 +0100

    asd

---
+++ FristLastRemove/FristLastRemove/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FristLastRemove
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kérem a sztringet: ");
+            string bemeno = Console.ReadLine();
+
+            Console.WriteLine($"A csonkolt string: {Remove_1(bemeno)}");
+
+            Console.WriteLine($"A csonkolt string: {Remove_2(bemeno)}");
+
+            Console.ReadKey(true);
+        }
+
+        public static string Remove_1(string Bemeno)
+        {
+            if (Bemeno.Length <= 2)
+            {
+                return Bemeno;
+            }
+            else
+            {
+                return Bemeno.Remove(0, 1).Remove(Bemeno.Length - 2, 1);
+            }
+
+
+        }
+            static string Remove_2(string Bemeno)
+        {
+                if (Bemeno.Length <= 2)
+                {
+                    return Bemeno;
+                }
+                else
+                {
+                    return Bemeno.Substring(1,Bemeno.Length -2);
+                }
+
+
+
+
+
+            }
+
+
+    }
+}
---
+++ FristLastRemove/FristLastRemove/Szoveg.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FristLastRemove
+{
+    class Szoveg
+    {
+
+        public string Bemeno
+        {
+            get;
+            set;
+        }
+
+        public Szoveg(string bemeno)
+        {
+            this.Bemeno = bemeno;
+
+        }
+
+
+        public static string Remove_1(string Bemeno)
+        {
+            if (Bemeno.Length <= 2)
+            {
+                return Bemeno;
+            }
+            else
+            {
+                return Bemeno.Remove(0, 1).Remove(Bemeno.Length - 2, 1);
+            }
+
+            string Remove_2(Bemeno)
+        {
+                if (Bemeno.Length <= 2)
+                {
+                    return Bemeno;
+                }
+                else
+                {
+                    return Bemeno.Substring(1, Bemeno.Length - 2);
+                }
+
+
+
+
+
+            }
+
+
+        }
+    }
+}
+
